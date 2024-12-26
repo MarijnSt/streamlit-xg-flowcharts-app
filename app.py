f8bf7dd79beefa550d039d156b442db6753089a8
MarijnSt <marijn.stammeleer@gmail.com>
2024-12-26 18:35:06 +0100

    feat: selectbox for teams

--- app.py
+++ app.py
@@ -1,3 +1,38 @@
 import streamlit as st
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 
-st.title("KAA Gent xG Flowcharts")
+st.title("Belgian Pro League xG Flowcharts")
+
+# GET TEAM LIST
+competition_url = "https://fbref.com/en/comps/37/Belgian-Pro-League-Stats"
+response = requests.get(competition_url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+table = soup.find('table', {'id': 'results2024-2025371_overall'})
+teams_data = []
+
+for row in table.find("tbody").find_all("tr"):
+    team_cell = row.find("td", {"data-stat": "team"})
+    team_name = team_cell.text.strip()
+    if team_name:
+        team_url = team_cell.find("a")["href"]
+        team_logo = team_cell.find("img")["src"]
+        teams_data.append({
+            "team_name": team_name, 
+            "team_url": f"https://fbref.com/{team_url}", 
+            "team_logo": team_logo
+        })
+
+teams_df = pd.DataFrame(teams_data)
+
+selected_team = st.selectbox(
+    "Select a team", 
+    teams_df["team_name"].tolist(),
+    index=None
+)
+
+if selected_team:
+    team_url = teams_df[teams_df["team_name"] == selected_team]["team_url"].values[0]
+    st.write(team_url)
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-This is a single page Streamlit app that generates xg flowcharts for KAA Gent games in the Belgian Pro League.
+This is a single page Streamlit app that generates xg flowcharts for teams in the Belgian Pro League.
 
 The data is scraped from fbref.
