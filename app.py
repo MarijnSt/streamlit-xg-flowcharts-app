b4c4df98fac0a6461551218aa55daf4149be39bc
MarijnSt <marijn.stammeleer@gmail.com>
2024-12-26 19:41:48 +0100

    feat: get matches data

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import streamlit as st
 import pandas as pd
 import requests
@@ -33,6 +34,50 @@ selected_team = st.selectbox(
     index=None
 )
 
+# GET MATCHES LIST
 if selected_team:
+    today = datetime.now().date()
     team_url = teams_df.loc[teams_df["team_name"] == selected_team]["team_url"].values[0]
-    st.write(team_url)
+    response = requests.get(team_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    table = soup.find('table', {'id': 'matchlogs_for'})
+    matches_data = []
+    
+    for row in table.find("tbody").find_all("tr"):
+        # Get match date
+        date_element = row.find("th", {"data-stat": "date"})
+        match_date_str = date_element.get("csk")
+        match_date = datetime.strptime(match_date_str, '%Y%m%d').date()
+        
+        # Get competition
+        competition_element = row.find("td", {"data-stat": "comp"})
+        competition = competition_element.text.strip()
+
+        # Only add matches from the Pro League that have been played
+        if (match_date < today and competition == "Pro League A"):
+            # Get match opponent
+            match_opponent = row.find("td", {"data-stat": "opponent"}).text.strip()
+
+            # Get venue
+            match_venue_str = row.find("td", {"data-stat": "venue"}).text.strip()
+            match_venue = "(H)" if "Home" in match_venue_str else "(A)"
+
+            # Get goals
+            goals_for = row.find("td", {"data-stat": "goals_for"}).text.strip()
+            goals_against = row.find("td", {"data-stat": "goals_against"}).text.strip()
+            score = f"{goals_for} - {goals_against}" if match_venue == "(H)" else f"{goals_against} - {goals_for}"
+
+            # Add to matches_list
+            #matches_list.append(f"{match_date} {match_opponent} {match_venue} {score}")
+            matches_data.append({
+                "match_date": match_date,
+                "match_opponent": match_opponent,
+                "match_venue": match_venue,
+                "score": score
+            })
+    
+    selected_match = st.selectbox(
+        "Select a match", 
+        [f"{match['match_date']} {match['match_opponent']} {match['match_venue']} {match['score']}" for match in matches_data],
+        index=None
+    )
