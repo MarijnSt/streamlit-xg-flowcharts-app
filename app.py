fac43aff17a1e37debe154a19cf0be9b6804e7e6
MarijnSt <marijn.stammeleer@gmail.com>
2024-12-27 20:24:33 +0100

    feat: show xg flowchart

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import os
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 
 st.title("Belgian Pro League xG Flowcharts")
 
@@ -103,57 +106,10 @@ if selected_team and selected_match:
     match_report_link = match_data["match_report_link"]
     response = requests.get(match_report_link)
     soup = BeautifulSoup(response.text, 'html.parser')
-    st.write(selected_team)
-    st.write(match_data)
-    st.write(match_report_link)
-
-    # GET SUMMARY DATA
 
     # Get team info
     home_team = selected_team if match_data["match_venue"] == "(H)" else match_data["match_opponent"]
     away_team = selected_team if match_data["match_venue"] == "(A)" else match_data["match_opponent"]
-    STATIC_DIR = os.path.join(os.path.dirname(__file__), "static")
-    home_logo = os.path.join(STATIC_DIR, f"logo-{home_team.lower()}.png")
-    away_logo = os.path.join(STATIC_DIR, f"logo-{away_team.lower()}.png")
-    st.write(home_team)
-    st.image(home_logo, width=100)
-    st.write(away_team)
-    st.image(away_logo, width=100)
-
-    # Get summary events
-    events_wrap_div = soup.find("div", {"id": "events_wrap"})
-    events_list = []
-    summary_events = events_wrap_div.find_all("div", {"class": "event"})
-    for event in summary_events:
-        # init event type
-        event_type = None
-        # check for goal
-        if "goal" in event.text.lower():
-            event_type = "Goal"
-        # check for own goal
-        if "own goal" in event.text.lower():
-            event_type = "Own goal"
-        # check for penalty goal
-        if "penalty kick" in event.text.lower():
-            event_type = "Goal"
-        # check for red card
-        if "red card" in event.text.lower() or "second yellow card" in event.text.lower():
-            event_type = "Red card"
-
-        if event_type:
-            event_minute = event.find("div").text.strip().split("’")[0].split("+")[0]
-            player_name = event.find("a").text.strip()
-            team = home_team if event.get("class")[1] == "a" else away_team
-            events_list.append({
-                "event_minute": event_minute,
-                "event_type": event_type,
-                "player_name": player_name,
-                "team_name": team
-            })
-
-    events_df = pd.DataFrame(events_list)
-    st.subheader("Summary events")
-    st.write(events_df)
 
     # GET SHOTS DATA
     all_shots_df = pd.read_html(match_report_link, attrs={"id": "shots_all"}, header=1)[0]
@@ -163,8 +119,6 @@ if selected_team and selected_match:
     shots_df.columns = ["event_minute", "player_name", "team_name", "xg", "outcome"]
     # Handle extra time shots
     shots_df["event_minute"] = shots_df["event_minute"].apply(lambda x: int(x.split("+")[0]) if "+" in x else int(x))
-    st.subheader("Shots")
-    st.write(shots_df)
 
     def create_team_shots_df(shots_df, team_name):
         # Filter for team, sort by minute and reset index
@@ -193,7 +147,132 @@ if selected_team and selected_match:
     # Create dataframes for home and away teams
     home_shots_df = create_team_shots_df(shots_df, home_team)
     away_shots_df = create_team_shots_df(shots_df, away_team)
-    st.subheader("Home shots")
-    st.write(home_shots_df)
-    st.subheader("Away shots")
-    st.write(away_shots_df)
+
+    # GET SUMMARY EVENTS
+    events_wrap_div = soup.find("div", {"id": "events_wrap"})
+    events_list = []
+    summary_events = events_wrap_div.find_all("div", {"class": "event"})
+    for event in summary_events:
+        # init event type
+        event_type = None
+        # check for goal
+        if "goal" in event.text.lower():
+            event_type = "Goal"
+        # check for own goal
+        if "own goal" in event.text.lower():
+            event_type = "Own goal"
+        # check for penalty goal
+        if "penalty kick" in event.text.lower():
+            event_type = "Goal"
+        # check for red card
+        if "red card" in event.text.lower() or "second yellow card" in event.text.lower():
+            event_type = "Red card"
+
+        if event_type:
+            event_minute = int(event.find("div").text.strip().split("’")[0].split("+")[0])
+            player_name = event.find("a").text.strip()
+            team = home_team if event.get("class")[1] == "a" else away_team
+            shots_df = home_shots_df if team == home_team else away_shots_df
+            cumulative_xg = shots_df.loc[shots_df["event_minute"] <= int(event_minute), "cumulative_xg"].max()
+
+            events_list.append({
+                "event_minute": event_minute,
+                "event_type": event_type,
+                "player_name": player_name,
+                "team_name": team,
+                "cumulative_xg": cumulative_xg
+            })
+
+    events_df = pd.DataFrame(events_list)
+
+    # CREATE FLOWCHART
+    background_color = "#f2f4ee"
+    black_color = "#0a0c08"
+    grey_color = "#7D7C84"
+
+    # init flowchart
+    fig, ax = plt.subplots(figsize = (10, 5))
+    fig.set_facecolor(background_color)
+    ax.set_facecolor(background_color)
+    plt.rcParams['font.family'] = 'Gill Sans'
+    plt.rcParams.update({
+        'text.color': black_color,
+        'axes.labelcolor': grey_color,
+        'axes.edgecolor': grey_color,
+        'xtick.color': grey_color,
+        'ytick.color': grey_color,
+        'grid.color': grey_color,
+    })
+
+    # team data
+    home_color = "#4c2484"
+    away_color = "#004794"
+
+    # plt customizations
+    plt.xticks([0, 15, 30, 45, 60, 75, 90])
+    plt.xlabel("Minute")
+    plt.ylabel("Cumulative xG")
+    plt.grid(True, alpha=0.2)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    # display the team names and score
+    ax.text(0.35, 1.15, home_team, color=home_color, fontsize=16, ha='right', transform=ax.transAxes)
+    ax.text(0.5, 1.15, match_data["score"], fontsize=16, ha='center', transform=ax.transAxes)
+    ax.text(0.65, 1.15, away_team, color=away_color, fontsize=16, ha='left', transform=ax.transAxes)
+
+    # game information
+    ax.text(0.5, 1.10, match_data["match_date"].strftime("%d-%m-%Y"), alpha=0.6, fontsize=9, ha="center", transform=ax.transAxes)
+    ax.text(0.5, 1.06, "Jupiler Pro League", alpha=0.6, fontsize=9, ha="center", transform=ax.transAxes)
+
+    # logo's
+    home_logo = mpimg.imread(f"static/logo-{home_team.lower()}.png")
+    home_imagebox = OffsetImage(home_logo, zoom=0.45)
+    home_ab = AnnotationBbox(home_imagebox, (0.05, 1.15), xycoords='axes fraction', frameon=False)
+    ax.add_artist(home_ab)
+    away_logo = mpimg.imread(f"static/logo-{away_team.lower()}.png")
+    away_imagebox = OffsetImage(away_logo, zoom=0.45)
+    away_ab = AnnotationBbox(away_imagebox, (0.95, 1.15), xycoords='axes fraction', frameon=False)
+    ax.add_artist(away_ab)
+
+    # xG for teams
+    home_total_xg = home_shots_df["cumulative_xg"].max()
+    away_total_xg = away_shots_df["cumulative_xg"].max()
+    ax.text(0.35, 1.10, f"{home_total_xg:.2f} xG", color=home_color, alpha=0.6, fontsize=9, ha='right', transform=ax.transAxes)
+    ax.text(0.65, 1.10, f"{away_total_xg:.2f} xG", color=away_color, alpha=0.6, fontsize=9, ha='left', transform=ax.transAxes)
+
+    # xG steps
+    ax.step(x = home_shots_df["event_minute"], y = home_shots_df["cumulative_xg"], where="post", color=home_color)
+    ax.step(x = away_shots_df["event_minute"], y = away_shots_df["cumulative_xg"], where="post", color=away_color)
+
+    # add event markers
+    def add_event_markers(df, team_name, team_color):
+        team_events_df = df.loc[df["team_name"] == team_name]
+        for _, row in team_events_df.iterrows():
+            # goals
+            if row["event_type"] == "Goal" or row["event_type"] == "Own goal":
+                # mark goal on xG chart
+                ax.scatter(x = row["event_minute"], y = row["cumulative_xg"], color = team_color, marker = "o")
+                # annotate goal
+                alpha_value = 1 if row["event_type"] == "Goal" else 0.5
+                text = row["player_name"]
+                if row["event_type"] == "Own goal":
+                    text += " (OG)"
+                    ax.annotate(text, (row["event_minute"], row["cumulative_xg"]), color=team_color, alpha=alpha_value, fontsize=8, ha="right", xytext=(-5, 7.5), textcoords="offset points")
+            # red cards
+            if row["event_type"] == "Red card":
+                ax.axvline(x = row["event_minute"], color = "red", linestyle = "--", linewidth = 1.1, alpha = 0.2)
+                ax.text(
+                    x=row["event_minute"] + 1, y= 0.63,
+                    s=row["player_name"],
+                    color="red",
+                    fontsize=9,
+                    ha='left',
+                    alpha= 0.5
+                )
+
+
+    add_event_markers(events_df, home_team, home_color)
+    add_event_markers(events_df, away_team, away_color)
+    # show plot
+    st.pyplot(fig)
